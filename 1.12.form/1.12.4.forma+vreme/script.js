0dfee6dc2cefd3b16833c69349ff60032f717e3a
nikola <nikolamilanovic2305@gmail.com>
2022-12-13 14:44:17 +0100

    1 more class

--- 1.12.form/1.12.4.forma+vreme/script.js
+++ 1.12.form/1.12.4.forma+vreme/script.js
@@ -1,7 +1,3 @@
-document.querySelector(`#submit`).addEventListener(`click`, e =>{
-    let name = document.formUser.name.value;
-    let lastname = document.formUser.lastname.value;
-    let grade = Number(document.formUser.grade.value);
-    let date =  document.formUser.date.value;
-    alert(`${name} ${lastname} je dana ${date} dobio/la ocenu ${grade}`);
+document.querySelector(`select`).addEventListener(`change`, e =>{
+    document.querySelector(`#data`).style.border = document.querySelector(`#border`).value    
 })
